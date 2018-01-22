11edd635e655f456d029a17225b39e077402cf06
Artemij Shepelev <ffelian@gmail.com>
2018-01-22 11:11:05 +0300

    added comment

--- response.go
+++ response.go
@@ -5,5 +5,7 @@ import "net/http"
 // Response is a response for a request
 type Response struct {
 	*http.Response
+
+	// ProxyAddr is a proxy address via which a request was performed
 	ProxyAddr string
 }
